b7d1d0bda54f8e1e33b45585e4903e739348d966
AMOREM <amorem@amorem.com>
2019-08-29 12:29:38 +0200

    Detail page done

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 Netflix UI like, but for Games :-)
 ![homepage](./screenshots/homepage.png)
+![detail](./screenshots/detail.png)
--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../widgets/gradient_background.dart';
 import '../widgets/topbar.dart';
+import '../data.dart';
 
 class DetailPage extends StatelessWidget {
   final String title;
@@ -28,7 +29,16 @@ class DetailPage extends StatelessWidget {
                 Color.fromRGBO(35, 45, 59, 1),
               ),
             ),
-            _infoColumn(context),
+            topBar(context),
+            Padding(
+              padding: EdgeInsets.fromLTRB(
+                MediaQuery.of(context).size.width * 0.030,
+                MediaQuery.of(context).size.height * 0.08,
+                MediaQuery.of(context).size.width * 0.030,
+                MediaQuery.of(context).size.width * 0.005,
+              ),
+              child: _infoColumn(context),
+            ),
           ],
         ),
       ),
@@ -55,7 +65,6 @@ class DetailPage extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisSize: MainAxisSize.max,
       children: <Widget>[
-        topBar(_context),
         SizedBox(
           height: MediaQuery.of(_context).size.height * 0.25,
         ),
@@ -68,9 +77,74 @@ class DetailPage extends StatelessWidget {
             }
           }).join(),
           style: TextStyle(
-              color: Colors.white, fontWeight: FontWeight.w500, fontSize: 34),
+              color: Colors.white, fontWeight: FontWeight.w500, fontSize: 32),
         ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(
+              0,
+              MediaQuery.of(_context).size.height * 0.02,
+              0,
+              MediaQuery.of(_context).size.height * 0.02),
+          child: Text(
+            'EA Games',
+            style: TextStyle(
+              fontSize: 16.0,
+              color: Colors.red[900],
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+        _infoText(),
+        Padding(
+          padding: EdgeInsets.fromLTRB(
+              0,
+              MediaQuery.of(_context).size.height * 0.04,
+              0,
+              MediaQuery.of(_context).size.height * 0.02),
+          child: Text(
+            'Snapshot',
+            style: TextStyle(
+              fontSize: 15.0,
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+        _snapshotsList(_context, screenshots),
       ],
+    );
+  }
+
+  Widget _infoText() {
+    return Text(
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \n\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      style: TextStyle(color: Colors.white, fontWeight: FontWeight.w200),
+    );
+  }
+
+  Widget _snapshotsList(BuildContext _context, List<String> snapshots) {
+    return SizedBox(
+      height: MediaQuery.of(_context).size.height * 0.18,
+      width: MediaQuery.of(_context).size.width,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: snapshots.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: Container(
+              width: MediaQuery.of(_context).size.width * .50,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(snapshots[index]),
+                ),
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
